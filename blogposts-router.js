407171da0848bc048876dc3dfb2518e773e17388
Ricardo Zavala <rzavala192008@yahoo.com>
2018-11-25 19:57:15 -0800

    final commit

--- blogposts-router.js
+++ blogposts-router.js
@@ -7,16 +7,16 @@ const jsonParser = bodyParser.json();
 
 const {BlogPosts} = require('./models');
 
-BlogPosts.create('Packers are done', "Their record is 4-5-1, and they are not going to win out", "CheeseheadCraig", "11-17-2018");
-BlogPosts.create('Red Dead Redemption 2 Thoughts', "Is this game worth the hype? Should I buy it tonight", "Gamerguy", "11-22-2018");
-BlogPosts.create('I hate my life...', "I'm a loser with no friends. Can somebody please give me advice on how to get girls???!?!", "CozyTortoise", "10-31-2018");
+BlogPosts.create('Packers are done', "Their record is 4-5-1, and they are not going to win out", "CheeseheadCraig");
+BlogPosts.create('Red Dead Redemption 2 Thoughts', "Is this game worth the hype? Should I buy it tonight", "Gamerguy");
+BlogPosts.create('I hate my life...', "I'm a loser with no friends. Can somebody please give me advice on how to get girls???!?!", "CozyTortoise");
 
 router.get('/', (req, res) =>{
     res.json(BlogPosts.get());
 });
 
 router.post('/', jsonParser, (req, res) =>{
-  const requiredFields = ['title', 'content', 'author', 'publishDate'];
+  const requiredFields = ['title', 'content', 'author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -53,7 +53,7 @@ router.put('/:id', jsonParser, (req, res) => {
     return res.status(400).send(message);
   }
   console.log(`Updating shopping list item \`${req.params.id}\``);
-  const updatedItem = BlogPosts.update({
+  BlogPosts.update({
     id: req.params.id,
     title: req.body.title,
     content: req.body.content,
--- server.js
+++ server.js
@@ -12,11 +12,9 @@ app.use(express.static('public'));
 
 
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
-});
 
-app.use('/blogposts', blogPostsRouter);
+
+app.use('/blog-posts', blogPostsRouter);
 
 
 
